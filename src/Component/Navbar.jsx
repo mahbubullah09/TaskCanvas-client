714d8901b0a89cbabb713fdb91675a7ae8e248d4
Mahbubullah <mahbubullahpathan@gmail.com>
2023-12-22 01:08:49 +0600

    init

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>Nav Bar</h2>
+            <h2 className="my-8 font-extrabold text-red-800"> fdg</h2>
+        </div>
+    );
+};
+
+export default Navbar;
